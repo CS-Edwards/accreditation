66478225365f97c6f98f9981e7d8f7f7dab43a17
CS-Edwards <candaces.edwards@gmail.com>
2024-03-06 15:24:30 -1000

    refactor-- moved functions in utils.py

--- src/app/main.py
+++ src/app/main.py
@@ -1,51 +1,65 @@
 
 import os
+import sys
 import json
 import weaviate
 from dotenv import load_dotenv
 
-
-load_dotenv()
-
-#Weaviate Cloud Service URL (WCS)
-URL = os.getenv("WCSURL") 
-
-#WCS API Key
-APIKEY = os.getenv("WCSAPI")
-
-#OPENAI API Key
-OPENAI = os.getenv("OPENAIKEY")
-
-client = weaviate.Client(
-    url= URL,
-    auth_client_secret = weaviate.auth.AuthApiKey(APIKEY),
-    additional_headers ={
-        "X-OPENAI-Api-Key": OPENAI
-    }
-)
-
-if not client.is_live():
-    raise ValueError('Weaviate Client is not live')
-else:
-    print('Successfully connected to WCS')
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from utils.utils import connect_to_weaviate, load_schema_from_file
 
 
-#
-# Database Schema
-#
+# load_dotenv()
+
+# #Weaviate Cloud Service URL (WCS)
+# URL = os.getenv("WCSURL") 
+
+# #WCS API Key
+# APIKEY = os.getenv("WCSAPI")
+
+# #OPENAI API Key
+# OPENAI = os.getenv("OPENAIKEY")
+
+# client = weaviate.Client(
+#     url= URL,
+#     auth_client_secret = weaviate.auth.AuthApiKey(APIKEY),
+#     additional_headers ={
+#         "X-OPENAI-Api-Key": OPENAI
+#     }
+# )
+
+# if not client.is_live():
+#     raise ValueError('Weaviate Client is not live')
+# else:
+#     print('Successfully connected to WCS')
+
+
+# #
+# # Database Schema
+# #
     
-SCHEMA_FILE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "config", "schema_v0.json"))
+# SCHEMA_FILE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "config", "schema_v0.json"))
+
+# try:
+#     # Load chema from file
+#     with open(SCHEMA_FILE_PATH,'r') as schema_file:
+#         schema = json.load(schema_file)
+#         print(schema)
+
+# except FileNotFoundError:
+#     print(f'Error: Schema file {SCHEMA_FILE_PATH} not found')
+
 
 try:
-    # Load chema from file
-    with open(SCHEMA_FILE_PATH,'r') as schema_file:
-        schema = json.load(schema_file)
-        print(schema)
+    client = connect_to_weaviate()
+    schema = load_schema_from_file()
+    client.schema.delete_all()
+    client.schema.create(schema)
 
-except FileNotFoundError:
-    print(f'Error: Schema file {SCHEMA_FILE_PATH} not found')
+except Exception as e:
+    print(f'ExceptionL {e}')
 
-client.schema.delete_all()
-client.schema.create(schema)
+
+
 
 
